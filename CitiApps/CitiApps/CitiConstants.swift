229f7849b5b8493dedf6df0196e04faf740aeece
Urahaman, Mohamad Mujeeb [GCB-OT NE] <mu58995@citi.com>
2023-02-21 14:18:59 -0600

    Changed method channel name and added in costant file

---
+++ CitiApps/CitiApps/CitiConstants.swift
@@ -0,0 +1,10 @@
+//
+//  CitiConstants.swift
+//  CitiApps
+//
+//  Created by Mohamad Mujeeb Urahaman on 21/02/23.
+//
+
+struct CitiConstants {
+    static let method_channel_name: String = "com.maveric.citiacsdemo"
+}
