293041cf037ab978248230162b2a22c78cfb7d23
Balaji <balajibm@maveric-systems.com>
2023-03-01 15:47:30 +0530

    Adhoc chat updates

--- CitiApps/CitiApps/ChatController.swift
+++ CitiApps/CitiApps/ChatController.swift
@@ -27,7 +27,7 @@ class ChatController  {
         
         do{
             let credential = try CommunicationTokenCredential(
-                token: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmU3ODMxYTAwLWZlNTctNDkyNS1hMDZmLWZhYWY1ZTgwYzBkNF8wMDAwMDAxNy0zNjkxLWRmZWItZjg4My0wODQ4MjIwMGY2MzkiLCJzY3AiOjE3OTIsImNzaSI6IjE2Nzc1NjUzNzciLCJleHAiOjE2Nzc2NTE3NzcsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQiLCJyZXNvdXJjZUlkIjoiZTc4MzFhMDAtZmU1Ny00OTI1LWEwNmYtZmFhZjVlODBjMGQ0IiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTY3NzU2NTM3N30.o08-g-_RSD6oCWOxpwIBhWjxjNTozK1dCmSXMr1pL1h6I8A9JOlTCgfYmbHnkjPy0BcK1ZaYBNet1j2WbfpYD-rhWOQ9fmJBfNjuE_AIebPJwcr_-vZFtjv3RpEsIZ8Wk3RksLLjE4JpuDDKSllkpAaIfU9qIjaDKE_At7gmHEGmLoDa6khQcSFHczhO_6Q1MMV1IkdJ3KO5C2w981AidJUREsZb9caPynE8i3QhgxkLVh4Mvo4qy0bOx0NUy870DIZ7GVS7eZLr0TiiEwfwGYcw_Zezbcrhij0R7uNF83u8CnyRisTn623jUHAXdmBvc9IZ3FVVjgAWtId2QCYCHg"
+                token: "eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmU3ODMxYTAwLWZlNTctNDkyNS1hMDZmLWZhYWY1ZTgwYzBkNF8wMDAwMDAxNy0zYzQ5LTAwMTgtZTNjNy01OTNhMGQwMDg0MGIiLCJzY3AiOjE3OTIsImNzaSI6IjE2Nzc2NjEyNjQiLCJleHAiOjE2Nzc3NDc2NjQsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQiLCJyZXNvdXJjZUlkIjoiZTc4MzFhMDAtZmU1Ny00OTI1LWEwNmYtZmFhZjVlODBjMGQ0IiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTY3NzY2MTI2NH0.OGV9G4LPtTCXsO7HK0qnIACw4T5-uBJ-DhoU86gReJ1uKZunylApJZLTPJBcrp1wmj5co6Bnn7dlCJjr3iAI48PzYA3-m2Ot60dQQT1of4-R7b7wD_fzg6-66wDHv78tSvjIeilrHe6wnmurQRUNd0v1JYTBeeBuqXAcv7o7oaFz8YC1gcyA5rL29y9KBzdsJYfkfQZINupLjmSL7rQ6LNaLea77BJ_TRYJAOZs1JAnzi6N7eDr5dDnDICwvtU8D-MMkUqsgGr24K3n6r_mOV6GJXRxYTaR7BeqK1jFumyXYFv97jEM9Gwmbd_w5FBJ6ggD091SCoZIzZDq8BLsY8A"
             )
             let options = AzureCommunicationChatClientOptions()
 
@@ -40,7 +40,7 @@ class ChatController  {
                 topic: "Adhoc Chat Demo",
                 participants: [
                     ChatParticipant(
-                        id: CommunicationUserIdentifier("8:acs:e7831a00-fe57-4925-a06f-faaf5e80c0d4_00000017-3691-dfeb-f883-08482200f639"),
+                        id: CommunicationUserIdentifier("8:acs:e7831a00-fe57-4925-a06f-faaf5e80c0d4_00000017-3c49-0018-e3c7-593a0d00840b"),
                         displayName: "Janet Johnson"
                     ),
                 ]
@@ -63,13 +63,14 @@ class ChatController  {
     
     func startChatComposite() {
         
-        let communicationIdentifier = CommunicationUserIdentifier("8:acs:e7831a00-fe57-4925-a06f-faaf5e80c0d4_00000017-3691-dfeb-f883-08482200f639")
+        let communicationIdentifier = CommunicationUserIdentifier("8:acs:e7831a00-fe57-4925-a06f-faaf5e80c0d4_00000017-3c49-0018-e3c7-593a0d00840b")
         
         
         guard let communicationTokenCredential = try? CommunicationTokenCredential(
-            token:"eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmU3ODMxYTAwLWZlNTctNDkyNS1hMDZmLWZhYWY1ZTgwYzBkNF8wMDAwMDAxNy0zNjkxLWRmZWItZjg4My0wODQ4MjIwMGY2MzkiLCJzY3AiOjE3OTIsImNzaSI6IjE2Nzc1NjUzNzciLCJleHAiOjE2Nzc2NTE3NzcsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQiLCJyZXNvdXJjZUlkIjoiZTc4MzFhMDAtZmU1Ny00OTI1LWEwNmYtZmFhZjVlODBjMGQ0IiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTY3NzU2NTM3N30.o08-g-_RSD6oCWOxpwIBhWjxjNTozK1dCmSXMr1pL1h6I8A9JOlTCgfYmbHnkjPy0BcK1ZaYBNet1j2WbfpYD-rhWOQ9fmJBfNjuE_AIebPJwcr_-vZFtjv3RpEsIZ8Wk3RksLLjE4JpuDDKSllkpAaIfU9qIjaDKE_At7gmHEGmLoDa6khQcSFHczhO_6Q1MMV1IkdJ3KO5C2w981AidJUREsZb9caPynE8i3QhgxkLVh4Mvo4qy0bOx0NUy870DIZ7GVS7eZLr0TiiEwfwGYcw_Zezbcrhij0R7uNF83u8CnyRisTn623jUHAXdmBvc9IZ3FVVjgAWtId2QCYCHg") else {
+            token:"eyJhbGciOiJSUzI1NiIsImtpZCI6IjEwNiIsIng1dCI6Im9QMWFxQnlfR3hZU3pSaXhuQ25zdE5PU2p2cyIsInR5cCI6IkpXVCJ9.eyJza3lwZWlkIjoiYWNzOmU3ODMxYTAwLWZlNTctNDkyNS1hMDZmLWZhYWY1ZTgwYzBkNF8wMDAwMDAxNy0zYzQ5LTAwMTgtZTNjNy01OTNhMGQwMDg0MGIiLCJzY3AiOjE3OTIsImNzaSI6IjE2Nzc2NjEyNjQiLCJleHAiOjE2Nzc3NDc2NjQsInJnbiI6ImFtZXIiLCJhY3NTY29wZSI6ImNoYXQiLCJyZXNvdXJjZUlkIjoiZTc4MzFhMDAtZmU1Ny00OTI1LWEwNmYtZmFhZjVlODBjMGQ0IiwicmVzb3VyY2VMb2NhdGlvbiI6InVuaXRlZHN0YXRlcyIsImlhdCI6MTY3NzY2MTI2NH0.OGV9G4LPtTCXsO7HK0qnIACw4T5-uBJ-DhoU86gReJ1uKZunylApJZLTPJBcrp1wmj5co6Bnn7dlCJjr3iAI48PzYA3-m2Ot60dQQT1of4-R7b7wD_fzg6-66wDHv78tSvjIeilrHe6wnmurQRUNd0v1JYTBeeBuqXAcv7o7oaFz8YC1gcyA5rL29y9KBzdsJYfkfQZINupLjmSL7rQ6LNaLea77BJ_TRYJAOZs1JAnzi6N7eDr5dDnDICwvtU8D-MMkUqsgGr24K3n6r_mOV6GJXRxYTaR7BeqK1jFumyXYFv97jEM9Gwmbd_w5FBJ6ggD091SCoZIzZDq8BLsY8A") else {
             return
         }
+        print(self.threadId)
 
         self.chatAdapter = ChatAdapter(
             endpoint: "https://acscallingchatcomserv.communication.azure.com/",
